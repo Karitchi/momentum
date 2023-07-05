32c1359f18a994b7e726deb726f4f43aca057d4b
Karitchi <karichikarichikarichi@gmail.com>
2023-07-05 17:14:56 +0000

    Minor redesign of the authentication form and creation of the AuthForm.svelte component

---
+++ sveltekit/src/lib/components/AuthForm.svelte
@@ -0,0 +1,36 @@
+<script>
+    export let action;
+</script>
+
+<form
+    class="mt-5 p-5 w-50 m-auto body-tertiary border rounded shadow"
+    method="POST"
+>
+    <div class="mb-3">
+        <label for="email" class="form-label">Email address</label>
+        <input
+            type="email"
+            class="form-control"
+            value="test@mail.com"
+            id="email"
+            aria-describedby="emailHelp"
+            placeholder="example@email.com"
+            name="email"
+            required="true"
+        />
+    </div>
+    <div class="mb-3">
+        <label for="password" class="form-label">Password</label>
+        <input
+            type="password"
+            class="form-control"
+            value="password"
+            id="password"
+            placeholder="password"
+            name="password"
+            required="true"
+        />
+    </div>
+
+    <button type="submit" class="btn btn-primary w-100">{action}</button>
+</form>
--- sveltekit/src/routes/login/+page.svelte
+++ sveltekit/src/routes/login/+page.svelte
@@ -1,28 +1,6 @@
-<form class="mt-5" method="POST">
-    <div class="mb-3">
-        <label for="email" class="form-label">Email address</label>
-        <input
-            type="email"
-            class="form-control"
-            value="test@mail.com"
-            id="email"
-            aria-describedby="emailHelp"
-            placeholder="example@email.com"
-            name="email"
-            required="true"
-        />
-    </div>
-    <div class="mb-3">
-        <label for="password" class="form-label">Password</label>
-        <input
-            type="password"
-            class="form-control"
-            value="password"
-            id="password"
-            placeholder="password"
-            name="password"
-            required="true"
-        />
-    </div>
-    <button type="submit" class="btn btn-primary">Loggin</button>
-</form>
+<script>
+    import AuthForm from "$lib/components/AuthForm.svelte";
+    const action = "login";
+</script>
+
+<AuthForm {action} />
