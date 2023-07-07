62a1b100f1cb37862e1c1d68537e6ab20b8793b0
Karitchi <karichikarichikarichi@gmail.com>
2023-07-07 12:14:31 +0000

    add basic carousel for posts

--- sveltekit/src/lib/components/Navbar.svelte
+++ sveltekit/src/lib/components/Navbar.svelte
@@ -13,11 +13,61 @@
     }
 </script>
 
-<nav class="mt-3 navbar navbar-expand-lg bg-body-secondary rounded">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="/">Momentum</a>
+<div class="navbar bg-base-100">
+    <div class="flex-1">
+        <a class="btn btn-ghost normal-case text-xl">Momentum</a>
+    </div>
+    <div class="flex-none gap-2">
+        {#if !data.isUserConnected}
+            <div class="flex-1">
+                <a class="btn normal-case" href="/login">Login</a>
+            </div>
+            <div class="flex-1">
+                <a class="btn normal-case" href="/register">Register</a>
+            </div>
+        {:else}
+            <div class="dropdown dropdown-end">
+                <label tabindex="0" class="btn btn-ghost btn-circle avatar">
+                    <!-- <img src="" /> -->
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        class="w-6 h-6"
+                    >
+                        <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
+                        />
+                    </svg>
+                </label>
+                <ul
+                    tabindex="0"
+                    class="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
+                >
+                    <li>
+                        <a class="justify-between" href="#"> Profile </a>
+                    </li>
+                    <li>
+                        <form method="POST">
+                            <button type="submit" class="link" value="Logout">
+                                Logout</button
+                            >
+                        </form>
+                    </li>
+                </ul>
+            </div>
+        {/if}
+    </div>
+</div>
+
+<!-- <nav>
+    <div>
+        <a href="/">Momentum</a>
         <button
-            class="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#navbarSupportedContent"
@@ -27,7 +77,7 @@
         >
             <span class="navbar-toggler-icon" />
         </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <div>
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                 {#if !data.isUserConnected}
                     <li class="nav-item">
@@ -51,4 +101,4 @@
             </ul>
         </div>
     </div>
-</nav>
+</nav> -->
