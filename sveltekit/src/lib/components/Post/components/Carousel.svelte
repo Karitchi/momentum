1357e8c33ed2c18835f5b789155c494aa7c7cb09
Karitchi <karichikarichikarichi@gmail.com>
2023-07-20 12:00:41 +0000

    Allow users to upload multiple images in a single post, fetching posts is broken for now

--- sveltekit/src/lib/components/Post/components/Carousel.svelte
+++ sveltekit/src/lib/components/Post/components/Carousel.svelte
@@ -22,44 +22,4 @@
         </div>
     <!-- {/each} -->
 
-    <!-- <div id="slide1" class="carousel-item relative w-full justify-center">
-        <img
-            alt="truc"
-            src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.snut.fr%2Fwp-content%2Fuploads%2F2015%2F08%2Fimage-de-paysage.jpg&f=1&nofb=1&ipt=8b4cf34e6bea2cef5b92990621ecc5ee01d6ac059b831d5f3f58c494c890437b&ipo=images"
-            class="object-contain"
-        />
-        <div
-            class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2"
-        >
-            <a href="#slide3" class="btn btn-square">❮</a>
-            <a href="#slide2" class="btn btn-square">❯</a>
-        </div>
-    </div> -->
 </div>
-<!-- <div id="slide2" class="carousel-item relative w-full justify-center">
-    <img
-        alt="truc"
-        src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.YAXlTjvtEKchdMVc5laZhwHaE8%26pid%3DApi&f=1&ipt=ac5e2bcb98574739f5e3eb2b160ad770c65295a417f86a1d4acf88f3bcf6a138&ipo=images"
-        class="object-contain"
-    />
-    <div
-        class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2"
-    >
-        <a href="#slide1" class="btn btn-square">❮</a>
-        <a href="#slide3" class="btn btn-square">❯</a>
-    </div>
-</div>
-
-<div id="slide3" class="carousel-item relative w-full justify-center">
-    <img
-        alt="truc"
-        src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.snut.fr%2Fwp-content%2Fuploads%2F2015%2F08%2Fimage-de-paysage.jpg&f=1&nofb=1&ipt=8b4cf34e6bea2cef5b92990621ecc5ee01d6ac059b831d5f3f58c494c890437b&ipo=images"
-        class="object-contain"
-    />
-    <div
-        class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2"
-    >
-        <a href="#slide2" class="btn btn-square">❮</a>
-        <a href="#slide1" class="btn btn-square">❯</a>
-    </div>
-</div> -->
--- sveltekit/src/lib/utils/post.js
+++ sveltekit/src/lib/utils/post.js
@@ -1,38 +1,41 @@
 import { getCookies, isUserConnected } from '$lib/utils/accountManagement'
-import { error, redirect } from '@sveltejs/kit'
-import { debug } from 'console';
+import { redirect } from '@sveltejs/kit'
 import { writeFile } from 'fs/promises';
-import { path } from 'path'
 
 export async function post(event) {
+
     if (! await isUserConnected(event)) throw redirect(300, '/login')
 
     const { userId } = await getCookies(event.cookies)
 
     const formData = await event.request.formData();
-    const image = await formData.get("image")
+    const images = await formData.getAll("image")
     const caption = await formData.get("caption")
 
-    const fileUUID = crypto.randomUUID()
-    const fileExt = image.name.split('.').pop();
-    const url = fileUUID + "." + fileExt
+    let fileUUID, fileExt, url
+    
+    for (const image of images) {
+        fileUUID = crypto.randomUUID()
+        fileExt = image.name.split('.').pop();
+        url = fileUUID + "." + fileExt
 
-    // store file to fs
-    writeFile(`./public/posts/${url}`, await image.stream());
+        // store file to fs
+        writeFile(`./public/posts/${url}`, await image.stream());
 
-    let text = `
-        INSERT INTO posts (user_id, caption)
-        VALUES ($1, $2)
-        RETURNING post_id
-    `
-    let values = [userId, caption]
-    const { rows } = await event.locals.pool.query(text, values)
-    text = `
-        INSERT INTO images (post_id, url)
-        VALUES ($1, $2)
-    `
-    values = [rows[0].post_id, url]
-    event.locals.pool.query(text, values)
+        let text = `
+            INSERT INTO posts (user_id, caption)
+            VALUES ($1, $2)
+            RETURNING post_id
+        `
+        let values = [userId, caption]
+        const { rows } = await event.locals.pool.query(text, values)
+        text = `
+            INSERT INTO images (post_id, url)
+            VALUES ($1, $2)
+        `
+        values = [rows[0].post_id, url]
+        event.locals.pool.query(text, values)
+    }
 }
 
 export async function getPosts(request) {
