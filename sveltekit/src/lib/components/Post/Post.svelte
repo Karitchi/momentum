2bc5e088789047ae2f7f9e6567a2f5d5520f001f
Karitchi <karichikarichikarichi@gmail.com>
2023-07-21 11:06:35 +0000

    display the post images

--- sveltekit/src/lib/components/Post/Post.svelte
+++ sveltekit/src/lib/components/Post/Post.svelte
@@ -5,14 +5,10 @@
     export let data;
 </script>
 
-<div class="grid justify-items-center">
+<div class="m-auto w-1/2 ">
     {#each data.posts as post}
-        <div class="border rounded-xl shadow-xl p-3 w-1/2 h-auto mb-4">
-            <!-- todo add poster info -->
-            <Carousel imageURLs={post.url} />
-            <Reaction />
-            <Message caption={post.caption} />
-            <!-- todo add date -->
-        </div>
+        <Carousel urls={post.urls} />
+        <Reaction />
+        <Message message={post.message} />
     {/each}
 </div>
--- sveltekit/src/lib/components/Post/components/Carousel.svelte
+++ sveltekit/src/lib/components/Post/components/Carousel.svelte
@@ -1,25 +1,17 @@
 <script>
-    export let imageURLs
+    export let urls;
 </script>
 
-<div
-    class="flex carousel carousel-center border rounded-xl bg-black"
->
-    <!-- {#each imageUrls as imageUrl} -->
-        <div id="slide1" class="carousel-item relative w-full justify-center">
-            <img
-                alt="truc"
-                src="/public/posts/{imageURLs}"
-
-                class="object-contain"
-            />
+<div class="carousel carousel-center border rounded-xl bg-black">
+    {#each urls as url, i}
+        <div id={i} class="carousel-item relative w-full justify-center">
+            <img alt="truc" src="/public/posts/{url}" class="object-contain" />
             <div
                 class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2"
             >
-                <a href="#slide3" class="btn btn-square">❮</a>
-                <a href="#slide2" class="btn btn-square">❯</a>
+                <a href="#{i - 1}" class="btn btn-square">❮</a>
+                <a href="#{i + 1}" class="btn btn-square">❯</a>
             </div>
         </div>
-    <!-- {/each} -->
-
+    {/each}
 </div>
